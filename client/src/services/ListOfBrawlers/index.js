07ac462946dd47b08a67bcb400f87465bf733c4b
Raúl <ramaque01@gmail.com>
2022-06-04 19:41:44 +0100

    creating a service of brawlers list

---
+++ client/src/services/ListOfBrawlers/index.js
@@ -0,0 +1,86 @@
+let item = {}
+item.name = "Brawler"
+
+export const listOfEpic = [
+    item = { 
+        name: 'epic'
+    },
+    item = {
+        name: 'Home'
+    },
+    item = { 
+        name: 'Home'
+    },
+]
+
+export const listOfMythic = [
+    item = { 
+        name: 'mitico'
+    },
+    item = {
+        name: 'Home'
+    },
+    item = { 
+        name: 'Home'
+    },
+]
+
+export const listOfChromatic = [
+    item = { 
+        name: 'chromatic'
+    },
+    item = {
+        name: 'Home'
+    },
+    item = { 
+        name: 'Home'
+    },
+]
+
+export const listOfTrophies = [
+    item = { 
+        name: 'epico'
+    },
+    item = {
+        name: 'Home'
+    },
+    item = { 
+        name: 'Home'
+    },
+]
+
+export const listOfLegendary = [
+    item = { 
+        name: 'legendary'
+    },
+    item = {
+        name: 'Home'
+    },
+    item = { 
+        name: 'Home'
+    },
+]
+
+export const listOfSuperRare = [
+    item = { 
+        name: 'epico'
+    },
+    item = {
+        name: 'Home'
+    },
+    item = { 
+        name: 'Home'
+    },
+]
+
+export const listOfRare = [
+    item = { 
+        name: 'epico'
+    },
+    item = {
+        name: 'Home'
+    },
+    item = { 
+        name: 'Home'
+    },
+]
