7aeed2de025d7d1b4f04db1cfe19df606741e19d
Raúl <ramaque01@gmail.com>
2022-06-04 19:41:18 +0100

    creating a service of categories

---
+++ client/src/services/ArrayList/index.js
@@ -0,0 +1,35 @@
+let item = {}
+item.name = "rarity"
+
+export const listOfCategories = [
+    item = {
+        id: 1,    
+        name: 'chromatic',
+        css: 'text-red-500'
+    },
+    item = {
+        id: 2,    
+        name: 'legendary',
+        css: 'text-blue-500'
+    },
+    item = {
+        id: 3,    
+        name: 'epic',
+        css: 'text-yellow-500'
+    },
+    item = {
+        id: 4,    
+        name: 'superRare',
+        css: 'text-yellow-500'
+    },
+    item = {
+        id: 5,    
+        name: 'rare',
+        css: 'text-yellow-500'
+    },
+    item = {
+        id: 6,    
+        name: 'trophies',
+        css: 'text-yellow-500'
+    }
+]
