cf9c45d496da028bdfb018fbd1c4684af7847f4e
Raúl <ramaque01@gmail.com>
2022-06-07 21:24:07 +0100

    refactoring Card component

--- root-project/src/client/components/Card/index.js
+++ root-project/src/client/components/Card/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from "react"; 
 
 export default function Card(params) {
+  const image = 
+  require(`../../../server/images/brawlers/${params.rarity}/${params.name}.png`)
+
   return (
-    <article key={`brawler${params.index}`} className="card">
-      <img src={params.image} alt={params.name} className='image-card'/>
+    <article key={`${params.index}brawler${params.name}`} className="card">
+      <img src={image} alt={params.name} className='image-card'/>
       <p className="foot-card">{params.name}</p>
     </article>
   );
