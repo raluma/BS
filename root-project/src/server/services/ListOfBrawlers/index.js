85611b18c1d7fb501115149a7778126b36de0e6d
Raúl <ramaque01@gmail.com>
2022-06-07 21:25:18 +0100

    refactoring data for brawlers

--- root-project/src/server/services/ListOfBrawlers/index.js
+++ root-project/src/server/services/ListOfBrawlers/index.js
@@ -1,60 +1,6 @@
 let brawler = {}
 brawler.name = "Brawler"
 
-export const listOfEpic = [
-    brawler = { 
-        name: 'Bea'
-    },
-    brawler = {
-        name: 'Bibi'
-    },
-    brawler = {
-        name: 'Edgar'
-    },
-    brawler = { 
-        name: 'Frank'
-    },
-    brawler = { 
-        name: 'Griff'
-    },
-    brawler = { 
-        name: 'Grom'
-    },
-    brawler = {
-        name: 'Nani'
-    },
-    brawler = {
-        name: 'Pam'
-    },
-    brawler = { 
-        name: 'Piper'
-    }
-]
-
-export const listOfMythic = [
-    brawler = { 
-        name: 'Byron'
-    },
-    brawler = { 
-        name: 'Gene'
-    },
-    brawler = { 
-        name: 'Max'
-    },
-    brawler = { 
-        name: 'Mortis'
-    },
-    brawler = {
-        name: 'Mr.p'
-    },
-    brawler = {
-        name: 'Sprout'
-    },
-    brawler = { 
-        name: 'Tara'
-    }
-]
-
 export const listOfChromatic = [
     brawler = { 
         name: 'Ash'
@@ -94,7 +40,121 @@ export const listOfChromatic = [
     }
 ]
 
+export const listOfEpic = [
+    brawler = { 
+        name: 'Bea'
+    },
+    brawler = {
+        name: 'Bibi'
+    },
+    brawler = {
+        name: 'Edgar'
+    },
+    brawler = { 
+        name: 'Frank'
+    },
+    brawler = { 
+        name: 'Griff'
+    },
+    brawler = { 
+        name: 'Grom'
+    },
+    brawler = {
+        name: 'Nani'
+    },
+    brawler = {
+        name: 'Pam'
+    },
+    brawler = { 
+        name: 'Piper'
+    }
+]
+
+export const listOfLegendary = [
+    brawler = { 
+        name: 'Amber'
+    },
+    brawler = {
+        name: 'Crow'
+    },
+    brawler = { 
+        name: 'Leon'
+    },
+    brawler = { 
+        name: 'Meg'
+    },
+    brawler = { 
+        name: 'Sandy'
+    },
+    brawler = { 
+        name: 'Spike'
+    }
+]
+
+export const listOfMythic = [
+    brawler = { 
+        name: 'Byron'
+    },
+    brawler = { 
+        name: 'Gene'
+    },
+    brawler = { 
+        name: 'Max'
+    },
+    brawler = { 
+        name: 'Mortis'
+    },
+    brawler = {
+        name: 'Mr.P'
+    },
+    brawler = {
+        name: 'Sprout'
+    }, 
+    brawler = { 
+        name: 'Squeak'
+    },
+    brawler = { 
+        name: 'Tara'
+    }
+]
+
+export const listOfRare = [
+    brawler = { 
+        name: 'Barley'
+    },
+    brawler = {
+        name: 'El Primo'
+    },
+    brawler = { 
+        name: 'Poco'
+    }
+]
+
+export const listOfSuperRare = [
+    brawler = { 
+        name: 'Carl'
+    },
+    brawler = {
+        name: 'Darryl'
+    },
+    brawler = { 
+        name: 'Jacky'
+    },
+    brawler = { 
+        name: 'Penny'
+    },
+    brawler = {
+        name: 'Rico'
+    },
+    brawler = { 
+        name: 'Rosa'
+    }
+]
+
 export const listOfTrophies = [
+    brawler = { 
+        name: '8-Bit'
+    },
     brawler = { 
         name: 'Bo'
     },
@@ -127,65 +187,6 @@ export const listOfTrophies = [
     },
     brawler = { 
         name: 'Tick'
-    },
-    brawler = { 
-        name: '8-bit'
     }
 ]
 
-export const listOfLegendary = [
-    brawler = { 
-        name: 'Amber'
-    },
-    brawler = {
-        name: 'Crow'
-    },
-    brawler = { 
-        name: 'Leon'
-    },
-    brawler = { 
-        name: 'Meg'
-    },
-    brawler = { 
-        name: 'Sandy'
-    },
-    brawler = { 
-        name: 'Spike'
-    },
-    brawler = { 
-        name: 'Squeak'
-    }
-]
-
-export const listOfSuperRare = [
-    brawler = { 
-        name: 'Carl'
-    },
-    brawler = {
-        name: 'Darryl'
-    },
-    brawler = { 
-        name: 'Jacky'
-    },
-    brawler = { 
-        name: 'Penny'
-    },
-    brawler = {
-        name: 'Rico'
-    },
-    brawler = { 
-        name: 'Rosa'
-    }
-]
-
-export const listOfRare = [
-    brawler = { 
-        name: 'Barley'
-    },
-    brawler = {
-        name: 'El Primo'
-    },
-    brawler = { 
-        name: 'Poco'
-    }
-]
