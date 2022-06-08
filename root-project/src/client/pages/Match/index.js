6b1288185e78d62dbf3b8c56ed16c3a585d1db3a
Raúl <ramaque01@gmail.com>
2022-06-08 23:51:31 +0100

    changing rarity menu

--- root-project/src/client/pages/Match/index.js
+++ root-project/src/client/pages/Match/index.js
@@ -5,36 +5,49 @@ import { listOfCategories } from "../../../server/services/ArrayList";
 import * as rarity from "../../../server/services/ListOfBrawlers";
 
 export default function Match() {
-  const [list, setList] = useState({rarity: "Epic", array: rarity.listOfEpic});
+  const [list, setList] = useState({
+    rarity: "Epic",
+    array: rarity.listOfEpic,
+  });
 
   const changeList = (name) => {
-    if (name === "Chromatic") setList({rarity: name, array: rarity.listOfChromatic});
-    else if (name === "Mythic") setList({rarity: name, array: rarity.listOfMythic});
-    else if (name === "Legendary") setList({rarity: name, array: rarity.listOfLegendary});
-    else if (name === "Epic") setList({rarity: name, array: rarity.listOfEpic});
-    else if (name === "SuperRare") setList({rarity: name, array: rarity.listOfSuperRare});
-    else if (name === "Rare") setList({rarity: name, array: rarity.listOfRare});
-    else setList({rarity: name, array: rarity.listOfTrophies});
+    if (name === "C")
+      setList({ rarity: "Chromatic", array: rarity.listOfChromatic });
+    else if (name === "M")
+      setList({ rarity: "Mythic", array: rarity.listOfMythic });
+    else if (name === "L")
+      setList({ rarity: "Legendary", array: rarity.listOfLegendary });
+    else if (name === "E")
+      setList({ rarity: "Epic", array: rarity.listOfEpic });
+    else if (name === "S")
+      setList({ rarity: "SuperRare", array: rarity.listOfSuperRare });
+    else if (name === "R")
+      setList({ rarity: "Rare", array: rarity.listOfRare });
+    else setList({ rarity: "Trophies", array: rarity.listOfTrophies });
   };
 
   return (
     <div className="Page-Match">
-      <Menu
-        orientation="H"
-        list={listOfCategories}
-        cssMenu="border-2 border-gray-900"
-        articleClick={(param) => changeList(param)}
-      />
 
       <section key="score">
         <p className="scoreboard">scoreboard</p>
       </section>
 
-      <section key='body' className="body">
-        {list.array.map((item, index) => (
-          <Card index={index} name={item.name} rarity={list.rarity} />
-        ))}
-      </section>
+      <div className="container">
+        <Menu
+          orientation="V"
+          list={listOfCategories}
+          cssMenu="border-2 border-gray-900 w-[16%] h-[532px]"
+          articleClick={(param) => changeList(param)}
+        />
+
+        <section key="body" className="body">
+          {list.array.map((item, index) => (
+            <Card index={index} name={item.name} rarity={list.rarity} />
+          ))}
+        </section>
+      </div>
+
     </div>
   );
 }
